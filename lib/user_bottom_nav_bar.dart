66f706be18658cd0a411e50c09890a2490e21e59
YunSE@525 <limyx0525@gmail.com>
2024-12-02 17:16:38 +0800

    initial version of bottom nav bar

---
+++ lib/user_bottom_nav_bar.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:fyp_recipe/user_home_page.dart';
+import 'package:fyp_recipe/user_mealplan_page.dart';
+import 'package:fyp_recipe/user_favourite_page.dart';
+import 'package:fyp_recipe/user_profile_page.dart';
+import 'package:fyp_recipe/user_search_page.dart';
+
+class UserBottomNavBar extends StatelessWidget {
+  final int currentIndex;
+
+  const UserBottomNavBar({super.key, required this.currentIndex});
+
+  void _onItemTapped(BuildContext context, int index) {
+    if (index == currentIndex) return; // Avoid reloading the same page.
+    Widget page;
+    switch (index) {
+      case 0:
+        page = const UserHomePage();
+        break;
+      case 1:
+        page = const UserSearchPage();
+        break;
+      case 2:
+        page = const UserMealplanPage();
+        break;
+      case 3:
+        page = const UserFavouritePage();
+        break;
+      case 4:
+        page = const UserProfilePage();
+        break;
+      default:
+        page = const UserHomePage();
+    }
+    Navigator.of(context).pushReplacement(
+      MaterialPageRoute(builder: (context) => page),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      currentIndex: currentIndex,
+      onTap: (index) => _onItemTapped(context, index),
+
+      backgroundColor: Colors.black,
+      selectedItemColor: Colors.greenAccent[400],
+      unselectedItemColor: Colors.black,
+      items: const [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home),
+          label: 'Home',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.search),
+          label: 'Search',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.calendar_today),
+          label: 'Meal Plan',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.favorite),
+          label: 'Favourites',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.person),
+          label: 'Profile',
+        ),
+      ],
+    );
+  }
+}
