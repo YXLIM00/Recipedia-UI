bdae08b8626efd9ff7bbce5816c765a24b89ec35
YunSE@525 <limyx0525@gmail.com>
2024-12-02 17:16:06 +0800

    change the background image

--- lib/background_image_container.dart
+++ lib/background_image_container.dart
@@ -3,21 +3,21 @@ import 'package:flutter/material.dart';
 class BackgroundContainer extends StatelessWidget {
   final Widget child;
 
-  const BackgroundContainer({required this.child, Key? key}) : super(key: key);
+  const BackgroundContainer({required this.child, super.key});
 
   @override
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
         image: DecorationImage(
-          image: AssetImage('images/uibackground.jpg'),
+          image: AssetImage('images/backgroundimage_jar.jpg'),
           fit: BoxFit.fill,
         ),
       ),
       child: Stack(
         children: [
           Container(
-            color: Colors.black.withOpacity(0.6), // Dark overlay with transparency
+            color: Colors.black.withOpacity(0.1), // Dark overlay with transparency
           ),
           child, // Content of each page
         ],
